b6b4fafa24bb6f1b6c39d7ce08b93a104edd0ebd
Tomas Franco <tomas.f.carvalho@gmail.com>
2023-01-22 00:00:12 -0300

    Aula 4 - integracao com o backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import './styles/global.css';
+import './libs/dayjs';
+
 import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
 
-import './styles/global.css';
 
 export function App(){
   return(
