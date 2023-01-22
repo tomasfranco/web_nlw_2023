3a65eeeec91f1972c8d75cb2a8a8ec273b5a2a81
Tomas Franco <tomas.f.carvalho@gmail.com>
2023-01-22 17:56:29 -0300

    Aula 05 - Features finalizadas com sucesso NLW e com adicao de acessibilidade do proprio radixui

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -9,7 +9,7 @@ export function ProgressBar(props: ProgressBarProps) {
         role="progressbar"
         aria-label="Progresso de hábitos completados neste dia"
         aria-valuenow={75}
-        className="h-3 rounded-xl bg-violet-600 w-3/4"
+        className="h-3 rounded-xl bg-violet-600 w-3/4 transition-all"
         style={{ width: `${props.progress}%` }}
       />
     </div>
